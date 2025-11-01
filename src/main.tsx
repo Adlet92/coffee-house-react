35534ccb9e6e6dfcb421216a32dd682925fb4794
adletb7 <adlet.baiteliyev@gmail.com>
2025-11-01 21:26:52 +0500

    feat: add registration page react

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import { CartProvider } from "./components/Card/cardContext";
 import CartPage from "./components/Card/CardPage";
 import MenuPage from "./components/Menu/MenuPage";
+import RegistrationPage from "./components/Registration/RegistrationPage";
 import SignInPage from "./components/SignIn/SignInPage";
 import "./styles.css";
 
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/register" element={<RegistrationPage/>} />
       </Routes>
     </Router>
     </CartProvider>
