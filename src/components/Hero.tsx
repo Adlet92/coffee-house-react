6cf074258af07c72e7e5c9d6778438865a4ba993
adletb7 <adlet.baiteliyev@gmail.com>
2025-11-20 08:39:52 +0500

    fix: hero part menu button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Hero: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleMenuClick = () => {
+    navigate("/menu");
+  };
   return (
     <section className="hero">
       <div className="hero-content">
@@ -15,9 +21,9 @@ const Hero: React.FC = () => {
           House is the perfect destination for coffee lovers seeking a relaxing
           experience.
         </p>
-        <a href="/menu-page/menu.html" className="hero-btn">
+        <button className="hero-btn" onClick={handleMenuClick}>
           Menu
-        </a>
+        </button>
       </div>
 
       <video
